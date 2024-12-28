dfd44e10bbc57a1c4627628ebfc6489d5cba53b4
Darthmoth <138891073+Darthmoth@users.noreply.github.com>
2024-12-28 13:25:41 -0500

    Added the additional files

---
+++ app-files/data.js
@@ -0,0 +1,391 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gate-entrance-inside",
+      "name": "Gate Entrance (inside)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.9587734711382456,
+        "pitch": -0.0002945013735136115,
+        "fov": 1.334719009654728
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9519270714895836,
+          "pitch": 0.2912183261718706,
+          "rotation": 0.7853981633974483,
+          "target": "4-gym-pools"
+        },
+        {
+          "yaw": -3.1009744249852567,
+          "pitch": 0.1597564181027913,
+          "rotation": 0,
+          "target": "1-between-vida-and-wigmore"
+        },
+        {
+          "yaw": 0.2554450637546797,
+          "pitch": 0.1649682877113996,
+          "rotation": 0,
+          "target": "3-far-side-of-wigmore-lake"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-between-vida-and-wigmore",
+      "name": "between vida and wigmore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.9960687192378916,
+        "pitch": 0.06442397742126715,
+        "fov": 1.334719009654728
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.207945756827108,
+          "pitch": 0.1995886153087607,
+          "rotation": 5.497787143782138,
+          "target": "5-hacienda-infront"
+        },
+        {
+          "yaw": 2.508109363908348,
+          "pitch": 0.19068382680291407,
+          "rotation": 0,
+          "target": "6-infront-of-the-outdoor-gym"
+        },
+        {
+          "yaw": -2.9131330690020008,
+          "pitch": 0.23311030742079097,
+          "rotation": 0,
+          "target": "5-hacienda-infront"
+        },
+        {
+          "yaw": 0.15042099182871027,
+          "pitch": 0.25130866248574435,
+          "rotation": 0,
+          "target": "0-gate-entrance-inside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6979654420591466,
+          "pitch": -0.15468954971355053,
+          "title": "VIda",
+          "text": "This is our Care team department where we have 24/7 nurses on standby waiting to assist!&nbsp;"
+        },
+        {
+          "yaw": 1.0330548010474327,
+          "pitch": 0.013748443009047762,
+          "title": "Wigmore Hall",
+          "text": "Welcome to Wigmore Hall, this is our dining hall as well as fabulous Lectures!&nbsp; If you have any questions the front desk can help!"
+        }
+      ]
+    },
+    {
+      "id": "2-crossway-by-laundry-and-yurts",
+      "name": "Crossway by Laundry and yurts",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1750863739346578,
+          "pitch": 0.23485935252507062,
+          "rotation": 0,
+          "target": "8-store-in-front"
+        },
+        {
+          "yaw": 2.9025287876033783,
+          "pitch": 0.17452177734219454,
+          "rotation": 0,
+          "target": "6-infront-of-the-outdoor-gym"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-far-side-of-wigmore-lake",
+      "name": "far side of wigmore lake",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03768541772799949,
+          "pitch": 0.05041927970967741,
+          "rotation": 0,
+          "target": "0-gate-entrance-inside"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-gym-pools",
+      "name": "Gym Pools",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32022003148115274,
+          "pitch": 0.0476694027327671,
+          "rotation": 0.7853981633974483,
+          "target": "1-between-vida-and-wigmore"
+        },
+        {
+          "yaw": -1.7449114671523915,
+          "pitch": 0.07952796728402767,
+          "rotation": 5.497787143782138,
+          "target": "0-gate-entrance-inside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0830905575906034,
+          "pitch": 0.06053226492960562,
+          "title": "Hippocrates Gym",
+          "text": "Welcome to our state of the art gym!&nbsp; Inside one&nbsp; can find all of our equipment and a huge suana and steam room!"
+        }
+      ]
+    },
+    {
+      "id": "5-hacienda-infront",
+      "name": "Hacienda (Infront)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.29207550747662836,
+        "pitch": 0.15472426746873325,
+        "fov": 1.334719009654728
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2703689442739918,
+          "pitch": 0.1565437565781771,
+          "rotation": 5.497787143782138,
+          "target": "8-store-in-front"
+        },
+        {
+          "yaw": -2.52916747775458,
+          "pitch": 0.1903310274316432,
+          "rotation": 0,
+          "target": "1-between-vida-and-wigmore"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6030034910321334,
+          "pitch": -0.18041185021085937,
+          "title": "Hacienda",
+          "text": "This is Hacienda! The first building of Hippocrates! Nowadays there are accomadations and a lecture hall inside!"
+        }
+      ]
+    },
+    {
+      "id": "6-infront-of-the-outdoor-gym",
+      "name": "infront of the outdoor gym",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.9727198982735104,
+        "pitch": 0.24183357696483831,
+        "fov": 1.334719009654728
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0036588083877707,
+          "pitch": 0.21997314170247506,
+          "rotation": 0,
+          "target": "2-crossway-by-laundry-and-yurts"
+        },
+        {
+          "yaw": 1.2143062541683616,
+          "pitch": 0.2072701710429019,
+          "rotation": 0,
+          "target": "1-between-vida-and-wigmore"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2300471241957283,
+          "pitch": 0.07076879025090577,
+          "title": "Salon &amp; Juice Bar",
+          "text": "Our virtual tour is under construction and will be full updated with all of our beautiful campus in no time!"
+        }
+      ]
+    },
+    {
+      "id": "7-pathway--outdoor-gym-to-laundry",
+      "name": "Pathway ~ Outdoor gym to Laundry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-store-in-front",
+      "name": "store in front",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.1870262401916527,
+        "pitch": 0.06669155436323138,
+        "fov": 1.334719009654728
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
